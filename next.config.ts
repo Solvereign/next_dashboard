f8c52917e51f1dd32551dd1c485f7d062e2d265c
Solvereign <bayarjavkhlan1123gmail.com>
2024-12-19 21:29:31 +0800

    10 ni bolohgui bna, odoo 11g hiine dee

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	/* config options here */
+	// experimental: {  // hamgiin suullin nextjs huwilbar deer l boldog gene ee
+	// 	ppr: 'incremental'
+	// }
 };
 
 export default nextConfig;
